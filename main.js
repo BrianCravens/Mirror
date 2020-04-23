758ff5c597081e9c5dde0851a402fb0cee68a796
Brian Cravens <brian.cravens303@gmail.com>
2020-04-23 17:04:51 -0500

    Finallygit add --all

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 let inputField = document.getElementById("message")
 let outputField1 = document.getElementById("article-1");
 let outputField2 = document.getElementById("article-2");
-// function handleKeyPress(event){
-//     outputField.innerHTML = event.target.value
-//     console.log("")
-// }
 
 inputField.addEventListener("keyup", function (event){
-    outputField1.innerHTML = event.target.value
-    outputField2.innerHTML = event.target.value
+    outputField1.textContent = event.target.value
+    outputField2.textContent = event.target.value
     console.log(event)
 })
