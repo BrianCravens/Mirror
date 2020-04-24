7d2023fd20d168894d655428c0265cd81027bbd5
Brian Cravens <brian.cravens303@gmail.com>
2020-04-24 11:37:17 -0500

    Updated w/for loop

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-let inputField = document.getElementById("message")
-let outputField1 = document.getElementById("article-1");
-let outputField2 = document.getElementById("article-2");
+// let inputField = document.getElementById("message")
+// let outputField1 = document.getElementById("article-1");
+// let outputField2 = document.getElementById("article-2");
+// let outputFieldAll = document.getElementsByClassName(".article-item")
 
-inputField.addEventListener("keyup", function (event){
-    outputField1.textContent = event.target.value
-    outputField2.textContent = event.target.value
-    console.log(event)
+// inputField.addEventListener("keyup", function (event){
+//     outputField1.textContent = event.target.value
+//     outputField2.textContent = event.target.value
+//     outputFieldAll.textContent = event.target.value
+//     console.log(event)
+// })
+
+document.querySelector("#message").addEventListener("keyup", () => {
+    let boxes = document.querySelectorAll(".article-item")
+    for( let i = 0; i < boxes.length;i++){
+        boxes[i].innerHTML = event.target.value
+    }
 })
